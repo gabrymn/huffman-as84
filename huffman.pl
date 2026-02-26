7aa61132ed20d4614ae3e9c386dc0abf4698f9c2
gabriele <gabrieledevs@gmail.com>
2026-02-26 21:55:15 +0100

    delete test

--- huffman.pl
+++ huffman.pl
@@ -1,25 +1,4 @@
-% Test di accettazione
-%-------------------------------------------------------------------------------
-program1(File) :-
-	hucodec_generate_sw(File, SWs, 1),
-	hucodec_generate_huffman_tree(SWs, HT),
-	hucodec_print_ht(HT).
-
-program2(Msg, Bits, HT, Save, SBs) :-
-	hucodec_generate_sw(Msg, SWs, 0),
-	hucodec_generate_huffman_tree(SWs, HT),
-	huffman_encode(Msg, HT, Bits),
-	hucodec_print_ht(HT),
-	hucodec_generate_symbol_bits_table(HT, SBs),
-	length(Bits, Len),
-	get_chars(Msg, Chars, 0),
-	length(Chars, NByte0),
-	NByte1 is (Len + 7) // 8,
-	Save is ((NByte0 - NByte1) / NByte0) * 100.
-
-%-------------------------------------------------------------------------------	
-	
-	
+		
 % Codifica partando da un messaggio di testo o dal contenuto di un file, che
 % viene interpretato come una stringa
 % Specifica tipi:
@@ -219,6 +198,7 @@ sort_elements(Elements, Sorted) :-
 
 
 % Print huffman tree
+% SPecifica tipi:
 %	HT: BinaryTree
 %-------------------------------------------------------------------------------
 hucodec_print_ht(HT) :-
